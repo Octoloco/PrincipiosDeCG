91eaa6b5faea942be8e9389752cfbc4a961db1be
Octoloco <diego_clorio@hotmail.com>
2018-03-25 12:44:22 -0600

    pe
    pequeños arreglos a midpointcircle

--- midPointCircle/src/Application.cpp
+++ midPointCircle/src/Application.cpp
@@ -72,6 +72,65 @@ void Application::clearScreen() {
 		}
 	}*/
 }
+float cirx1;
+float ciry1;
+float cirr1;
+
+float cirx2;
+float ciry2;
+float cirr2;
+
+float cirx3;
+float ciry3;
+float cirr3;
+
+float cirx4;
+float ciry4;
+float cirr4;
+
+float cirx5;
+float ciry5;
+float cirr5;
+
+float cirx6;
+float ciry6;
+float cirr6;
+
+float cirx7;
+float ciry7;
+float cirr7;
+
+float cirx8;
+float ciry8;
+float cirr8;
+
+float cirx9;
+float ciry9;
+float cirr9;
+
+float cirx10;
+float ciry10;
+float cirr10;
+
+float cirx11;
+float ciry11;
+float cirr11;
+
+float cirx12;
+float ciry12;
+float cirr12;
+
+float cirx13;
+float ciry13;
+float cirr13;
+
+float cirx14;
+float ciry14;
+float cirr14;
+
+float cirx15;
+float ciry15;
+float cirr15;
 
 
 
@@ -80,6 +139,8 @@ void Application::draw() {
 
 	float cx = WIDTH / 2;
 	float cy = HEIGHT / 2;
+
+	
 
 	/*for (int angulo = 0; angulo < 360; angulo++)
 	{
@@ -95,8 +156,22 @@ void Application::draw() {
 	//Poligono(4);
 
 
-
-	midPointCircle(cx, cy, 300);
+	
+	midPointCircle(cirx1, ciry1, cirr1);
+	midPointCircle(cirx2, ciry2, cirr2);
+	midPointCircle(cirx3, ciry3, cirr3);
+	midPointCircle(cirx4, ciry4, cirr4);
+	midPointCircle(cirx5, ciry5, cirr5);
+	midPointCircle(cirx6, ciry6, cirr6);
+	midPointCircle(cirx7, ciry7, cirr7);
+	midPointCircle(cirx8, ciry8, cirr8);
+	midPointCircle(cirx9, ciry9, cirr9);
+	midPointCircle(cirx10, ciry10, cirr10);
+	midPointCircle(cirx11, ciry11, cirr11);
+	midPointCircle(cirx12, ciry12, cirr12);
+	midPointCircle(cirx13, ciry13, cirr13);
+	midPointCircle(cirx14, ciry14, cirr14);
+	midPointCircle(cirx15, ciry15, cirr15);
 
 
 
@@ -147,7 +222,65 @@ void Application::setup() {
 
 	sierpinsky(a, b, c, 3);*/
 	
+	cirx1 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry1 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr1 = rand() % (600 - 50 + 1) + 50;
 
+	cirx2 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry2 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr2 = rand() % (600 - 50 + 1) + 50;
+
+	cirx3 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry3 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr3 = rand() % (600 - 50 + 1) + 50;
+
+	cirx4 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry4 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr4 = rand() % (600 - 50 + 1) + 50;
+
+	cirx5 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry5 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr5 = rand() % (600 - 50 + 1) + 50;
+
+	cirx6 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry6 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr6 = rand() % (600 - 50 + 1) + 50;
+
+	cirx7 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry7 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr7 = rand() % (600 - 50 + 1) + 50;
+
+	cirx8 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry8 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr8 = rand() % (600 - 50 + 1) + 50;
+
+	cirx9 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry9 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr9 = rand() % (600 - 50 + 1) + 50;
+
+	cirx10 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry10 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr10 = rand() % (600 - 50 + 1) + 50;
+
+	cirx11 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry11 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr11 = rand() % (600 - 50 + 1) + 50;
+
+	cirx12 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry12 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr12 = rand() % (600 - 50 + 1) + 50;
+
+	cirx13 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry13 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr13 = rand() % (600 - 50 + 1) + 50;
+
+	cirx14 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry14 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr14 = rand() % (600 - 50 + 1) + 50;
+
+	cirx15 = rand() % (WIDTH - 0 + 1) + 0;
+	ciry15 = rand() % (HEIGHT - 0 + 1) + 0;
+	cirr15 = rand() % (600 - 50 + 1) + 50;
 	
 	//Vcopy.push_back(vec4(-10, -10, 10));
 	//Vcopy.push_back(vec4(-10, 10, 10));
